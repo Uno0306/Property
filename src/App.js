ff27a9f9bfb3ac56a2585b735a5f290480139880
Uno0306 <zion6878@naver.com>
2021-06-09 22:41:43 +0900

    7thDeploy

--- src/App.js
+++ src/App.js
@@ -54,11 +54,11 @@ function App() {
         
       })}
       <div className="ooh">
-        <a href="main.html" id="gam" ><h1 className='gamja'>감자감자</h1></a>
+        <a href="index.html" id="gam" ><h1 className='gamja'>은행 전세금리</h1></a>
       </div>
       <div className="nav">
         <ul >
-          <a href="#"><li className="list">로그인</li></a>
+          <a href="index2.html"><li className="list">로그인</li></a>
           <a href="#"><li className="list">오</li></a>
           <a href="#"><li className="list">아</li></a>
           <a href="#"><li className="list">이</li></a>
--- src/index2.js
+++ src/index2.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Form from './Form';
 import reportWebVitals from './reportWebVitals';
 
+console.log("form Render 실행 전");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Form />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('form')
 );
+ console.log("form실행");
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
